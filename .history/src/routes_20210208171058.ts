911f8720f06ac140e170b1e9a8155c3eec7b2fdb
Artur Oliveira Gomes <>
2021-02-08 17:11:08 -0400

    error with cors

--- .history/src/routes_20210208171058.ts
+++ .history/src/routes_20210208171058.ts
@@ -8,7 +8,7 @@ import AlunoController from './controllers/AlunoController';
 import ChangeStatusController from './controllers/ChangeStatusController';
 import ChangeClassController from './controllers/ChangeClassController';
 import SessionController from './controllers/SessionController';
-// import authMiddleware from './middlewares/auth';
+import authMiddleware from './middlewares/auth';
 
 const routes = Router();
 const upload = multer(multerConfig);
