b2f567a7897a64b2839ea7b2d27f5bc4d70c14af
Artur Oliveira Gomes <>
2021-02-08 15:38:56 -0400

    adding sessions

--- .history/src/server_20210208153811.ts
+++ .history/src/server_20210208153811.ts
@@ -11,12 +11,19 @@ import './database/connection';
 const app = express();
 
 app.use(cors());
+app.use(cors({
+  origin: "https://app.professorsergiolima.com.br",
+  credentials: true
+}));
+app.options("*", cors());
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
 
-app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
-app.listen(3333, () => {
-  console.log('Server started!');
+
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+  console.log('Server running at http://127.0.0.1:%s', port);
 });
