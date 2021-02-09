692f52992d89511eec22b24ddf3c9c0dca739738
Artur Oliveira Gomes <>
2021-02-09 11:25:16 -0400

    erro no cors

--- .history/src/routes_20210209112503.ts
+++ .history/src/routes_20210209112503.ts
@@ -7,7 +7,7 @@ import TurmaListController from './controllers/TurmaListController';
 import AlunoController from './controllers/AlunoController';
 import ChangeStatusController from './controllers/ChangeStatusController';
 import ChangeClassController from './controllers/ChangeClassController';
-// import SessionController from './controllers/SessionController';
+import SessionController from './controllers/SessionController';
 // import authMiddleware from './middlewares/auth';
 
 const routes = Router();
