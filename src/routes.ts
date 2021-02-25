c269d6d18d12e5c5f7108db6c14eeed03cd2d5f7
Artur Oliveira Gomes <>
2021-02-25 09:29:23 -0400

    Revert "removing cors files - error in cors"
    This reverts commit 8b82f8a1726593ec2ff10809af8933d1267eaa95.

--- src/routes.ts
+++ src/routes.ts
@@ -7,8 +7,8 @@ import TurmaListController from './controllers/TurmaListController';
 import AlunoController from './controllers/AlunoController';
 import ChangeStatusController from './controllers/ChangeStatusController';
 import ChangeClassController from './controllers/ChangeClassController';
-// import SessionController from './controllers/SessionController';
-// import authMiddleware from './middlewares/auth';
+import SessionController from './controllers/SessionController';
+import authMiddleware from './middlewares/auth';
 
 const routes = Router();
 const upload = multer(multerConfig);
@@ -24,7 +24,7 @@ routes.post('/aluno/:id', AlunoController.show);
 routes.post('/aluno/status/:id/:status', ChangeStatusController.update);
 routes.post('/aluno/class/:id/:classe', ChangeClassController.update);
 routes.post('/aluno/update/:id', AlunoController.update);
-// routes.post('/sessions', SessionController.index);
-// routes.use(authMiddleware);
+routes.post('/sessions', SessionController.index);
+routes.use(authMiddleware);
 
 export default routes;
