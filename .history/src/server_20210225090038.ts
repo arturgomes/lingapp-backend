8f222abe12ead0471c3e1e0f860bab2413d94f4d
Artur Oliveira Gomes <>
2021-02-25 09:00:53 -0400

    error with cors while adding auth0

---
+++ .history/src/server_20210225090038.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import 'express-async-errors';
+
+import routes from './routes';
+import errorHandler from './errors/handler';
+
+import './database/connection';
+
+const app = express();
+
+
+app.use(express.json());
+
+app.use(cors());
+
+
+app.use(routes);
+app.use(errorHandler);
+
+
+
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+  console.log('Server running at http://127.0.0.1:%s', port);
+});
