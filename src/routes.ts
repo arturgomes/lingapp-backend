ee207c3b9ab8ca30ecfd4f9ffe23b5e7e9d9a3ac
Artur Oliveira Gomes <>
2021-05-12 18:36:38 -0400

    trying to find bug in obs and removing cors

--- src/routes.ts
+++ src/routes.ts
@@ -7,8 +7,6 @@ import TurmaListController from './controllers/TurmaListController';
 import AlunoController from './controllers/AlunoController';
 import ChangeStatusController from './controllers/ChangeStatusController';
 import ChangeClassController from './controllers/ChangeClassController';
-import SessionController from './controllers/SessionController';
-import authMiddleware from './middlewares/auth';
 
 const routes = Router();
 const upload = multer(multerConfig);
@@ -24,7 +22,5 @@ routes.post('/aluno/:id', AlunoController.show);
 routes.post('/aluno/status/:id/:status', ChangeStatusController.update);
 routes.post('/aluno/class/:id/:classe', ChangeClassController.update);
 routes.post('/aluno/update/:id', AlunoController.update);
-routes.post('/sessions', SessionController.index);
-routes.use(authMiddleware);
 
 export default routes;
